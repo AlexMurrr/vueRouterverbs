5b0007597ae06b214f3d95cfacf226b989b25e1c
Alex Mur <lehamuraschov2014@yandex.ru>
2023-08-07 22:29:04 +0200

    feat: add component 'buttonV and formVerb'

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import formVerb from './components/formVerb.vue';
 </script>
 
 <template>
  
-
+<formVerb/>
  
 </template>
 
-<style scoped>
-
+<style>
+    #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 15px auto;
+  width: 800px;
+  background-color: rgb(232, 232, 248);
+}
+body {
+  background-color: rgb(243, 240, 240);
+}
+input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  padding: 10px;
+  margin-bottom: 10px;
+  font-size: 15px;
+}
+button {
+  background-color: rgb(224, 231, 231);
+}
 </style>
---
+++ src/components/formVerb.vue
@@ -0,0 +1,52 @@
+<script setup>
+import  buttonV from "./buttonV.vue";
+</script>
+
+<template>
+
+  <div>
+    <form @submit.prevent="submit">
+      <label class="label"><strong>Translate:</strong></label> <br />
+
+      
+      
+      
+
+      <label class="label"><strong>Simple Past:</strong></label> <br />
+     
+      <inputV     
+    
+      />
+      
+      
+      <label class="label"><strong> Simple Participle:</strong></label> <br/>
+
+      
+
+      <buttonV
+        v-if="isTrue"
+        class="btn"
+        type="submit"
+        label="Start"
+        @click="(isTrue = !isTrue)"
+      />
+      
+      <buttonV
+        v-else
+        class="btn"
+        type="submit"
+        label="Check"
+        @click="
+          (isTrue = !isTrue)"
+      />      
+      <hr />    
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.label {
+  color: #17173f;
+}
+
+</style>
